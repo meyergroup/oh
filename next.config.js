d6f2edc5d6d2e7dda6b575f3816211f360d9f8dc
meyergroup <flixbot07@gmail.com>
2025-07-10 20:47:29 +0200

    Add static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
-  basePath: '/oh', // 👈 Replace with your repo name
+  basePath: '/oh', // 👈 Name of your GitHub repo
+  assetPrefix: '/oh', // 👈 Prefix for assets (CSS, JS, etc.)
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
